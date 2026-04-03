c22fdb5dc8439608ee9bb410e7d28c4bf4a79f4c
Manuel <Manuel.Kuss@uantwerpen.be>
2026-04-03 12:05:02 +0200

    added GitHub Actions workflows to deploy to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,11 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  // TODO: Update to your GitHub Pages or custom domain URL
+  // Examples:
+  //   GitHub Pages:   'https://username.github.io'
+  //   Custom domain:  'https://example-lab.com'
+  // See: https://docs.astro.build/en/guides/deploy/github/#configure-astro
+  site: 'https://bittremieuxlab.github.io',
+  integrations: [preact()],
 });
